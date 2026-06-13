f098cc5f2ec72648b32c15c972258338ff2bb04c
Rajan <chaudharyrajan721@gmail.com>
2026-06-13 19:34:26 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const fs = require("fs");
+const multer = require('multer');
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser");
+
+
+app.listen(3000, () => { console.log("server running...") })
